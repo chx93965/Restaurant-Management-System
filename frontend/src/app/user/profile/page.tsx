68bb5efe1ede43f5a3fb69c5bd67ca45a6e3acdc
changh28 <71030485+changh28@users.noreply.github.com>
2025-03-25 23:33:23 -0400

    restructure

--- frontend/src/app/user/profile/page.tsx
+++
@@ -1,7 +0,0 @@
-"use client";
-import React, { useEffect, useState } from 'react';
-import { useRouter } from 'next/router';
-import { getOwnedRestaurants } from '@/app/api/user';
-import { getUserInfo } from '@/app/api/user';  // Function to fetch user's info, can be added to user.ts API
-
-
---
+++ frontend/src/main.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import "./styles/app.css";
+
+import home from "./routes/home";
+import menu from "./routes/menu";
+import order from "./routes/order";
+import restaurant from "./routes/restaurant";
+import user from "./routes/user";
+
+
+const router = createBrowserRouter([
+    {
+        path: "/", element: <home />
+    },
+    {
+        path: "/menu", element: <menu />
+    },
+    {
+        path: "/order", element: <order />
+    },
+    {
+        path: "/restaurant", element: <restaurant />
+    },
+    {
+        path: "/user/:uid", element: <user />
+    }
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+    <React.StrictMode>
+        <RouterProvider router={router} />
+    </React.StrictMode>
+);
